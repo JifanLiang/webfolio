17136075af094360d022be69bfcf1789bbb9d603
Jifan Liang <liang.jifan@gmail.com>
2017-06-25 15:49:55 +0200

    Make menu white when scrolling

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,14 @@
+<script>
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    var $bandeau = $(".bandeau");
+    $nav.toggleClass('navbar-not-transparent', $(this).scrollTop() > $bandeau.height()-1);
+  });
+});
+</script>
 <header>
-					<nav id="main_menu">
+					<nav id="main_menu" class="navbar navbar-default navbar-fixed-top navbar-color-on-scroll navbar-transparent">
 						<nav id="menu_vertical">
 							<h1><a href="index.php">Jifan Liang</a></h1>
 							<ul id="vertical">
@@ -7,12 +16,12 @@
 								<li class="menu_mobile"><a href="a_propos.php" title="À propos">À-propos</a></li>
 								<li id="der" class="menu_mobile"><a href="contact.php" title="contact">Contact</a></li>
 								<li>
-									<a href="index.php#webdesign">
+									<a href="#webdesign">
 										Web-design
 									</a>
 								</li>
-								<li><a href="index.php#graphisme">Design graphique</a></li>
-								<li><a href="index.php#photographie">Photographie</a></li>
+								<li><a href="#graphisme">Design graphique</a></li>
+								<li><a href="#photographie">Photographie</a></li>
 								
 							</ul>
 						</nav>
